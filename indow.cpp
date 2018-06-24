33e7947b193ad2c15fd3cb2ceedcd952c6e74132
bigyellowtaxis <40375136+bigyellowtaxis@users.noreply.github.com>
2018-06-24 15:39:01 -0400

    added comments

--- indow.cpp
+++ indow.cpp
@@ -3,12 +3,12 @@
 #include <QPushButton>
 #include <QApplication>
 
-window::window(QWidget *parent) :
+window::window(QWidget *parent) :  //defines function?
     QWidget(parent)
 {
-    setFixedSize(100,50);
+    setFixedSize(100,50);  //creates window
 
-    m_button = new QPushButton("Hello", this);
-    m_button->setGeometry(10,10,80,30);
-    connect(m_button, SIGNAL (clicked()),QApplication::instance(), SLOT (quit()));
+    m_button = new QPushButton("Hello", this);  //creates button with text in window
+    m_button->setGeometry(10,10,80,30);  //sets size of button
+    connect(m_button, SIGNAL (clicked()),QApplication::instance(), SLOT (quit()));  //closes window when button is pushed
 }
